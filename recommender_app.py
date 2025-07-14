0107fbe99a2ba4e18dc83b2848fa6e45f1e13459
vishpuro <76851671+vishpuro@users.noreply.github.com>
2025-07-14 17:05:10 +0100

    Update recommender_app.py

--- recommender_app.py
+++ recommender_app.py
@@ -96,9 +96,9 @@ def predict(model_name, user_ids, params):
 	# Start making predictions based on model name, test user ids, and parameters
 	with st.spinner('Generating course recommendations: '):
 		time.sleep(0.5)
-      		res = backend.predict(model_name, user_ids, params)
-   	st.success('Recommendations generated!')
-   	return res
+		res = backend.predict(model_name, user_ids, params)
+	st.success('Recommendations generated!')
+	return res
 
 
 # ------ UI ------
@@ -120,32 +120,30 @@ st.sidebar.subheader('2. Tune Hyper-parameters: ')
 # Course similarity model
 if model_selection == backend.models[0]:
 	# Add a slide bar for selecting top courses
-   top_courses = st.sidebar.slider('Top courses',min_value=0, max_value=100,value=10, step=1)
-   # Add a slide bar for choosing similarity threshold
-   course_sim_threshold = st.sidebar.slider('Course Similarity Threshold %',min_value=0, max_value=100,value=50, step=10)
-   params['top_courses'] = top_courses
-   params['sim_threshold'] = course_sim_threshold
+	top_courses = st.sidebar.slider('Top courses',min_value=0, max_value=100,value=10, step=1)
+	# Add a slide bar for choosing similarity threshold
+	course_sim_threshold = st.sidebar.slider('Course Similarity Threshold %',min_value=0, max_value=100,value=50, step=10)
+	params['top_courses'] = top_courses
+	params['sim_threshold'] = course_sim_threshold
 	# TODO: Add hyper-parameters for other models
-# User profile model
+	# User profile model
 elif model_selection == backend.models[1]:
 	profile_sim_threshold = st.sidebar.slider('User Profile Similarity Threshold %',min_value=0, max_value=100,value=1, step=1)
-   params['profile_sim_threshold'] = profile_sim_threshold
-# Clustering model
+	params['profile_sim_threshold'] = profile_sim_threshold
+	# Clustering model
 elif (model_selection == backend.models[2])or(model_selection == backend.models[3]):
 	n_enrollments = st.sidebar.slider('Number of enrollments to count as a popular course',min_value=10, max_value=200,value=100, step=10)
-   params['n_enrollments'] = n_enrollments
+	params['n_enrollments'] = n_enrollments
 
 elif model_selection == backend.models[4]:
 	k_max = st.sidebar.slider('Define the K in K-Nearest Neighbours',min_value=10, max_value=100,value=40, step=10)
-   params['k_max'] = k_max
-
+	params['k_max'] = k_max
 
 # Training
 st.sidebar.subheader('3. Training: ')
 training_button = st.sidebar.button("Train Model")
 training_text = st.sidebar.text('')
 # Start training process
-
 
 # Prediction
 st.sidebar.subheader('4. Prediction')
@@ -154,20 +152,20 @@ st.sidebar.subheader('4. Prediction')
 pred_button = st.sidebar.button("Recommend New Courses")
 
 if pred_button and selected_courses_df.shape[0] > 0:
-   # Create a new id for current user session
+	# Create a new id for current user session
     
-   # Start training process
+	# Start training process
 	new_id = backend.add_new_ratings(selected_courses_df['COURSE_ID'].values)
     
-   #train(model_selection, params) 
+	#train(model_selection, params) 
     
-   user_ids = [new_id]
-   res_df = predict(model_selection, user_ids, params)
+	user_ids = [new_id]
+	res_df = predict(model_selection, user_ids, params)
 	if (model_selection == backend.models[2])or(model_selection == backend.models[3]):
-   	res_df = res_df[['COURSE_ID']]
+		res_df = res_df[['COURSE_ID']]
 	else:
-   	res_df = res_df[['COURSE_ID', 'SCORE']]
-	course_df = load_courses()
-	res_df = pd.merge(res_df, course_df, on=["COURSE_ID"]).drop('COURSE_ID', axis=1)
-	st.dataframe(res_df)
+		res_df = res_df[['COURSE_ID', 'SCORE']]
+		course_df = load_courses()
+		res_df = pd.merge(res_df, course_df, on=["COURSE_ID"]).drop('COURSE_ID', axis=1)
+		st.dataframe(res_df)
 
