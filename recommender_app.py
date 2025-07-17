cd8922a76e415f121bc0e055ed9fdcce35178216
vishpuro <76851671+vishpuro@users.noreply.github.com>
2025-07-17 17:13:51 +0100

    Update recommender_app.py

--- recommender_app.py
+++ recommender_app.py
@@ -163,13 +163,23 @@ st.sidebar.subheader('3. Train model and predict results')
 pred_button = st.sidebar.button("Recommend New Courses")
 
 if pred_button and selected_courses_df.shape[0] <= 0:
-	st.write("No courses selected. Here is a list of the top 20 rated courses")
-	ratings_df = load_ratings()
-	res_df=pd.DataFrame(ratings_df[['item','rating']].groupby(by=['item']).mean().sort_values(by='rating',ascending=False)).reset_index(drop=False).iloc[0:20,:]
-	res_df.rename(columns={"item":"COURSE_ID",'rating':"SCORE"},inplace=True)
-	course_df = load_courses()
-	res_df = pd.merge(res_df, course_df, on=["COURSE_ID"]).drop('COURSE_ID', axis=1)
-	st.dataframe(res_df)
+	rated, enrolled = st.tabs(["Top Courses by ratings","Top courses by number enrolled"])
+	with rated:
+		st.write("No courses selected. Here is a list of the top 20 rated courses")
+		ratings_df = load_ratings()
+		res_df=pd.DataFrame(ratings_df[['item','rating']].groupby(by=['item']).mean().sort_values(by='rating',ascending=False)).reset_index(drop=False).iloc[0:20,:]
+		res_df.rename(columns={"item":"COURSE_ID",'rating':"SCORE"},inplace=True)
+		course_df = load_courses()
+		res_df = pd.merge(res_df, course_df, on=["COURSE_ID"]).drop('COURSE_ID', axis=1)
+		st.dataframe(res_df,use_container_width=True)
+	with enrolled:
+		st.write("No courses selected. Here is a list of the top 20 rated courses")
+		ratings_df = load_ratings()
+		res_df=pd.DataFrame(ratings_df[['item','rating']].groupby(by=['item']).size().sort_values(ascending=False),columns=['SCORE']).reset_index(drop=False).iloc[0:20,:]
+		res_df.rename(columns={"item":"COURSE_ID"},inplace=True)
+		course_df = load_courses()
+		res_df = pd.merge(res_df, course_df, on=["COURSE_ID"]).drop('COURSE_ID', axis=1)
+		st.dataframe(res_df,use_container_width=True)
 
 elif pred_button and selected_courses_df.shape[0] > 0:
 	# Create a new id for current user session
