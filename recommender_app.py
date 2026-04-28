363af7a0a8be5a27a1375009c599a60a67982d62
vishpuro <76851671+vishpuro@users.noreply.github.com>
2026-04-28 19:51:20 +0100

    Fix update mode parameter in AgGrid call

--- recommender_app.py
+++ recommender_app.py
@@ -60,7 +60,7 @@ def init__recommender_app():
 		course_df,
 		gridOptions=grid_options,
 		enable_enterprise_modules=True,
-		update_mode=GridUpdateMode.MODEL_CHANGED,
+		update_on=GridUpdateMode.MODEL_CHANGED,
 		data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
 		fit_columns_on_grid_load=False,)
 	
