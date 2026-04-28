13cbfa029e2501f66ba9e1aa241a0e4be2852e92
vishpuro <76851671+vishpuro@users.noreply.github.com>
2026-04-28 20:19:13 +0100

    Fix prediction assignment to use list conversion

--- backend.py
+++ backend.py
@@ -798,10 +798,10 @@ def predict(model_name, user_ids, params):
 				pred=(pred*2)+3
 				
 				st.write("Outputting...")
-				st.write(np.shape(pred))
-				st.write(len(test_dataset.loc[:,'rating']))
+				#st.write(np.shape(pred))
+				#st.write(len(test_dataset.loc[:,'rating']))
 				
-				test_dataset.loc[:,'rating']=pred
+				test_dataset.loc[:,'rating']=list(pred)
 				res_df=test_dataset
 				res_df.sort_values(by='rating',ascending=False,inplace=True)
 				res_df.rename(columns={'user':'USER','item':'COURSE_ID','rating':'SCORE'},inplace=True)
